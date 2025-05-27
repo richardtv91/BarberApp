94be89e28ab2aed4dac0f70398afdde2f05d5fb3
master <master@SMLAB306PC14_25>
2025-05-27 17:35:07 -0500

    login interfaz para pantalla principal

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -31,6 +31,7 @@ public partial class LoginPage : ContentPage
             var responseData = await response.Content.ReadAsStringAsync();
             var persona = JsonConvert.DeserializeObject<Persona>(responseData);
             // Aquí puedes guardar el token o realizar otras acciones necesarias
+            await Navigation.PushAsync(new Views.PanelPrincipal());
             await DisplayAlert("Éxito", "Inicio de sesión exitoso", "OK");
             // Navegar a la página principal o realizar otra acción
         }
